ef738a89b6e56101480b2e2f635496ae1100efca
Yuriy13202 <ascendo@list.ru>
2020-09-15 02:18:12 +0700

    Updates

---
+++ precache-manifest.8cdd00721703ff9cf1d4ed8ac611df60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0be29a79e713e8ba0c4564a43b07cc4",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "625107c2c10a708ba6b4",
+    "url": "/aviasales/static/css/main.74f02572.chunk.css"
+  },
+  {
+    "revision": "16ba95d9ad7741044790",
+    "url": "/aviasales/static/js/2.992793a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aviasales/static/js/2.992793a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625107c2c10a708ba6b4",
+    "url": "/aviasales/static/js/main.fe1bcaf0.chunk.js"
+  },
+  {
+    "revision": "0cc15852d7adf8a9722e",
+    "url": "/aviasales/static/js/runtime-main.b4f11597.js"
+  },
+  {
+    "revision": "68ca57dc522eda771a31bb07bdd256b1",
+    "url": "/aviasales/static/media/check.68ca57dc.svg"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/aviasales/static/media/logo.0c089234.svg"
+  }
+]);
