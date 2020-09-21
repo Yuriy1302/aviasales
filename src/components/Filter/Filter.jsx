e8881fc8290f971f2e3e3ee17f6b82da5e5f9a1f
Yuriy13202 <ascendo@list.ru>
2020-09-21 21:38:14 +0700

    Filters first commit

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,25 +2,72 @@ import React from "react";
 
 import "./Filter.scss";
 
+/* const filters = [
+  { name: 'allTr', title: 'Все', isChecked: false },
+  { name: 'noTr', title: 'Без пересадок', isChecked: false },
+  { name: 'oneTr', title: '1 пересадка', isChecked: false },
+  { name: 'twoTr', title: '2 пересадки', isChecked: false },
+  { name: 'thrieTr', title: '3 пересадки', isChecked: false },
+]; */
+
+/* const filters = [
+  [ 'allTr', false ],
+  { name: 'noTr', isChecked: false },
+  { name: 'oneTr', isChecked: false },
+  { name: 'twoTr', isChecked: false },
+  { name: 'thrieTr', isChecked: false },
+];
+ */
+/* const Checkbox = (props) => {
+  const { name, isChecked, } = props;
+  render (
+    <>
+      <input type="checkbox" onChange={this.onSetAllCheckboxes} checked={this.state.allTr} id="all-check" name="allTr" />
+      <label htmlFor="all-check">Все</label>
+    </>
+  )
+} */
+
+
+
 class Filter extends React.Component {
 
   constructor() {
     super();
       this.state = {
-        allTr: false,
+        filterState: [
+          { id: 1, name: 'allTr', title: 'Все', isChecked: false },
+          { id: 2, name: 'noTr', title: 'Без пересадок', isChecked: false },
+          { id: 3, name: 'oneTr', title: '1 пересадка', isChecked: true },
+          { id: 4, name: 'twoTr', title: '2 пересадки', isChecked: false },
+          { id: 5, name: 'thrieTr', title: '3 пересадки', isChecked: false },
+        ],
+        /* allTr: false,
         noTr: false,
         oneTr: false,
         twoTr: false,
-        thrieTr: false,
+        thrieTr: false, */
       };
   }
 
-  filtered = (event) => {
-    event.preventDefault();
-    console.log(event.target.id);
+  handleCheckboxClick = (e) => {
+    
+    const name = e.target.name;
+    //const index = this.state.filterState.findIndex(item => item.name === name);
+    console.log(name);
+    //console.log(index);
+    
+
+    
+
   }
 
-  onSetAllCheckboxes = () => {
+/*   filtered = (event) => {
+    event.preventDefault();
+    console.log(event.target.id);
+  } */
+
+/*   onSetAllCheckboxes = () => {
     if (this.state.allTr) {
       this.setState({
         allTr: false,
@@ -39,9 +86,9 @@ class Filter extends React.Component {
       });
     }
 
-  }
+  } */
 
-  onSetOneCheckbox = (event) => {
+/*   onSetOneCheckbox = (event) => {
     const name = event.target.name;
     const n = this.state[name];
     
@@ -51,20 +98,23 @@ class Filter extends React.Component {
       
       this.setState({ [name]: true });
     }
-
-
-    
-    
-    /* console.log('name: ', name);
-    console.log('Значение: ', n); */
-    //this.setState({ [name]: !a });
-  }
+  } */
 
   render() {
 
     return (
-      <form className="form" onSubmit={this.filtered}>
-        <input type="checkbox" onChange={this.onSetAllCheckboxes} checked={this.state.allTr} id="all-check" name="allTr" />
+      <form className="form">
+        
+        {
+          this.state.filterState.map((item) => (
+            <div key={item.id}>
+              <input type="checkbox" onChange={this.handleCheckboxClick} checked={item.isChecked} id="all-check" name={item.name} />
+              <label htmlFor="all-check">{item.title}</label>
+            </div>
+          ))
+        }
+        
+        {/* <input type="checkbox" onChange={this.onSetAllCheckboxes} checked={this.state.allTr} id="all-check" name="allTr" />
         <label htmlFor="all-check">Все</label>
 
         <input type="checkbox" onChange={this.onSetOneCheckbox} checked={this.state.noTr} id="no-transfer" name="noTr" />
@@ -77,10 +127,24 @@ class Filter extends React.Component {
         <label htmlFor="2-transfers">2 пересадки</label>
 
         <input type="checkbox" onChange={this.onSetOneCheckbox} checked={this.state.thrieTr} id="3-transfers" name="thrieTr" />
-        <label htmlFor="3-transfers">3 пересадки</label>
+        <label htmlFor="3-transfers">3 пересадки</label> */}
       </form>
     );
   }
 };
+
+/* const Checkboxes = (props) => {
+  const { name, title, isChecked } = props.item;
+  const { handleCheckboxClick } = props;
+  console.log(name);
+  console.log(title);
+  console.log(isChecked);
+  return (
+    <div>
+      <input type="checkbox" onChange={handleCheckboxClick} checked={isChecked} data-name={name} id="all-check" name={name} />
+      <label htmlFor="all-check">{title}</label>
+    </div>
+  );
+} */
 
 export default Filter;
